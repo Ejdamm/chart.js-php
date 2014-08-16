9644dd9255f1756831c631e907eea01582c88be8
Hugo Heneault <in4matik@hotmail.fr>
2014-08-16 14:20:59 +0200

    Add PolarArea chart

--- chart.js.php
+++ chart.js.php
@@ -3,6 +3,7 @@ require 'class/ChartJS.php';
 require 'class/ChartJS_Line.php';
 require 'class/ChartJS_Bar.php';
 require 'class/ChartJS_Radar.php';
+require 'class/ChartJS_PolarArea.php';
 
 $array_values = array(array(65, 59, 80, 81, 56, 55, 40), array(28, 48, 40, 19, 86, 27, 90));
 $array_labels = array("January", "February", "March", "April", "May", "June", "July");
@@ -21,6 +22,16 @@ $Radar = new ChartJS_Radar('example_radar', 500, 500);
 $Radar->addRadar($array_values[0]);
 $Radar->addRadar($array_values[1]);
 $Radar->addLabels($array_labels);
+
+$PolarArea = new ChartJS_PolarArea('example_polararea', 500, 500);
+$PolarArea->addSegment(65);
+$PolarArea->addSegment(59);
+$PolarArea->addSegment(80);
+$PolarArea->addSegment(81);
+$PolarArea->addSegment(56);
+$PolarArea->addSegment(55);
+$PolarArea->addSegment(40);
+$PolarArea->addLabels($array_labels);
 
 ?><!DOCTYPE html>
 <html>
@@ -41,6 +52,11 @@ echo $Bar
 <h1>Radar</h1>
 <?php
 echo $Radar
+?>
+
+<h1>Polar Area</h1>
+<?php
+echo $PolarArea
 ?>
 <script src="Chart.js"></script>
 <script src="chart.js-php.js"></script>
---
+++ class/ChartJS_PolarArea.php
@@ -0,0 +1,37 @@
+<?php
+
+class ChartJS_PolarArea extends ChartJS
+{
+    protected $_type = 'PolarArea';
+
+    /**
+     * Add a set of data
+     * @param float $value
+     * @param array $options
+     * @param null $name Name cas be use to change data / options later
+     */
+    public function addSegment($value, $options = array(), $name = null)
+    {
+        if (!$name) {
+            $name = count($this->_datasets) + 1;
+        }
+        $this->_datasets[$name]['value'] = $value;
+        $this->_datasets[$name]['options'] = $options;
+    }
+
+    /**
+     * Prepare data (dataset and labels) for the PolarArea
+     */
+    protected function _renderData()
+    {
+        $array_data = array();
+        $i = 0;
+
+        foreach ($this->_datasets as $segment) {
+            $array_data[] = $segment['options'] + array('label' => $this->_labels[$i]) + array('value' => $segment['value']);
+            $i++;
+        }
+
+        return ' data-data=\'' . json_encode($array_data) . '\'';
+    }
+}
