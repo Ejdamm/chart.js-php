5202caf1b70d1fc7383a70584b8a41e21fec989c
Hugo Heneault <in4matik@hotmail.fr>
2014-08-16 15:18:14 +0200

    Add some default colors

--- chart.js.php
+++ chart.js.php
@@ -7,7 +7,11 @@ require 'class/ChartJS_PolarArea.php';
 require 'class/ChartJS_Pie.php';
 require 'class/ChartJS_Doughnut.php';
 
-ChartJS::addDefaultColor(array('fill' => 'rgba(220,220,220,0.2)', 'stroke' => 'rgba(220,220,220,1)', 'point' => 'rgba(220,220,220,1)', 'pointStroke' => '#fff'));
+ChartJS::addDefaultColor(array('fill' => '#f2b21a', 'stroke' => '#e5801d', 'point' => '#e5801d', 'pointStroke' => '#e5801d'));
+ChartJS::addDefaultColor(array('fill' => 'rgba(28,116,190,.8)', 'stroke' => '#1c74be', 'point' => '#1c74be', 'pointStroke' => '#1c74be'));
+ChartJS::addDefaultColor(array('fill' => 'rgba(212,41,31,.7)', 'stroke' => '#d4291f', 'point' => '#d4291f', 'pointStroke' => '#d4291f'));
+ChartJS::addDefaultColor(array('fill' => '#dc693c', 'stroke' => '#ff0000', 'point' => '#ff0000', 'pointStroke' => '#ff0000'));
+ChartJS::addDefaultColor(array('fill' => 'rgba(46,204,113,.8)', 'stroke' => '#2ecc71', 'point' => '#2ecc71', 'pointStroke' => '#2ecc71'));
 
 $array_values = array(array(65, 59, 80, 81, 56, 55, 40), array(28, 48, 40, 19, 86, 27, 90));
 $array_labels = array("January", "February", "March", "April", "May", "June", "July");
