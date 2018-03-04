dbdc52c49426b5af267a6fbe44c21a149c3b4540
Adam Georgsson <adamgeorgsson@gmail.com>
2018-03-04 21:45:14 +0100

    Fix for issue #3

--- js/driver.js
+++ js/driver.js
@@ -7,8 +7,7 @@ function loadChartJsPhp() {
 	var elements = document.querySelectorAll("[data-chartjs]");
 
 	// Looping every canvas
-	for (var canvas of elements)
-	{
+	for (var canvas of elements) {
 		var id = canvas.id;
 
 		// Getting ctx from canvas
@@ -19,9 +18,22 @@ function loadChartJsPhp() {
 		var type = htmldata.chartjs;
 		var data = JSON.parse(htmldata.data);
 		var options = JSON.parse(htmldata.options);
+		evalFunctions(options)
 		var config = {type:type, data:data, options:options};
 
 		// Creating chart and saving for later use
 		ChartJSPHP[id] = new Chart(ctx, config);
 	}
 };
+
+//Converts functions in json from strings to actual functions
+function evalFunctions(chartArray) {
+	for (var key in chartArray) {
+		var value = chartArray[key];
+		if (value instanceof Array || value instanceof Object ) {
+			evalFunctions(value);
+		} else if (typeof value === "string" && value.indexOf('function(') == 0) {
+			chartArray[key] = eval("(" + value + ")");
+		}
+	}
+}
